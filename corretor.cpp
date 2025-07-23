4e51435e823c13354165a5ee4f69181d45de5b39
Pedro-IoT <pedromaiadepaiva@gmail.com>
2025-07-22 22:47:03 -0300

    Update corretor.cpp

--- corretor.cpp
+++ corretor.cpp
@@ -1,4 +1,11 @@
 #include "corretor.h"
 
-Corretor::Corretor(int id, const std::string& telefone, bool avaliador, double lat, double lng, const std::string& nome)
-    : id(id), telefone(telefone), avaliador(avaliador), lat(lat), lng(lng), nome(nome) {}
+Corretor::Corretor(int id, const std::string& telefone, bool avaliador, double lat, double lng, const std::string& nome){
+    this -> id = id;
+    this -> telefone = telefone;
+    this -> avaliador = avaliador;
+    this -> lat = lat;
+    this -> lng = lng;
+    this -> nome = nome;
+}
+
